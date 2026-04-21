738a953524db36c5c180d44395cdb56c71c70988
kuri13254 <riqquu@gmail.com>
2026-04-21 10:57:05 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,28 @@
-#include <stdio.h>
+/*
+Soal 1 - Manuskrip Retak
+Nama : Thoriq Al Maududi
+NIM  : 13224054
+*/
 
-int main(void) {
-    return 0;
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    /*Deklarasi Variabel*/
+    char inpstr[] = "";
+
+    /*User Input*/
+    gets(inpstr);
+
+    /*Pemrosesan*/
+    for(int i=0 ; i<strlen(inpstr) ; i++){
+       if (inpstr[i] == '('){
+        if (inpstr[i+2] == ')' && inpstr[i]>='a' && inpstr[i]<= 'z'){
+            i+=2;
+        }
+       }
+    }
+
+    /*Output*/
+    printf("%s", inpstr);
 }
